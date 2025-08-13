b5a0de9461cde6cb7839c8ae20cab8051095fd9a
Toobyloba <balogun.tooby@gmail.com>
2025-08-13 23:31:09 +0800

    Commit on Wed Aug 13 11:31:09 PM CST 2025

--- AdminUser.java
+++ AdminUser.java
@@ -1,7 +1,13 @@
-public class AdminUser extends User{
+public class AdminUser extends User {
     private int accessLevel;
-    public AdminUser(String username, int userId, int accessLevel){
+
+    public AdminUser(String username, int userId, int accessLevel) {
         super(username, userId);
         this.accessLevel = accessLevel;
+    }
+
+    public void logBranchCleanUp(int branchesDeleted) {
+        System.out.println("Admin " + this.getUsername() + "ID: " + this.getUserId() + ", Level: " + this.accessLevel
+                + ") deleted " + branchesDeleted + "branches");
     }
 }
