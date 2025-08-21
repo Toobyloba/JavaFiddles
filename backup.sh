2f0523a5b2a95d177bb4818ebb88c7f7dabdde9d
Toobyloba <balogun.tooby@gmail.com>
2025-08-21 18:11:26 +0800

    Commit on Thu Aug 21 06:11:26 PM CST 2025

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]; then
 fi
 
 directory_name=$1
-tarfile_name=$1_$(date +"%Y-%m-%d").tar.gz
+tarfile_name=$(basename $1)_$(date +"%Y-%m-%d").tar.gz
 echo $tarfile_name
 tar -czvf $tarfile_name $1
