8e3e6c11ed8c3030ef5727200fdc02629ebd1fcc
Toobyloba <balogun.tooby@gmail.com>
2025-08-21 18:09:27 +0800

    Commit on Thu Aug 21 06:09:27 PM CST 2025

--- backup.sh
+++ backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "You need to specify a directory"
+    exit 1
+fi
+
 directory_name=$1
 tarfile_name=$1_$(date +"%Y-%m-%d").tar.gz
 echo $tarfile_name
-
+tar -czvf $tarfile_name $1
