63d71348217c6b91511dffdb8bcfc1ce281e69fd
Toobyloba <balogun.tooby@gmail.com>
2025-09-04 20:09:51 +0800

    Commit on Thu Sep  4 08:09:51 PM CST 2025

--- project/JavaFiddles/java/FilterExample.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.List;
-
-public class FilterExample{
-    public static void main(String[] args) {
-        List<String> tools = Arrays.asList("Git", "Gradle", "JUnit", "Java", "Jenkins");
-        tools.stream().filter(tool -> tool.startsWith("J")).forEach(System.out::println);
-    }
-}
--- project/JavaFiddles/scripts/labex_save_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config user.name "Toobyloba"
-git config user.email "balogun.tooby@gmail.com"
-current_date=$(date)
-git add .
-git commit -m "Commit on $current_date"
-git push
